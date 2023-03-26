baa07d4d1eefc4185521c25718c4b6c2a11f890c
agataniedzielko <119620895+agataniedzielko@users.noreply.github.com>
2023-03-26 20:13:10 +0200

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button2_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+private void button1_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = CreateGraphics())
+            {
+                g.DrawEllipse(Pens.Red, 100, 100, 50, 50);
+
+            }//
+        }
+
+
+        int x = 0;
+        int y = 0;
+        //int old_x = 0;
+        //int old_y = 0;
+        int krokx = 1;
+        int kroky = 1;
+
+        Bitmap b = new Bitmap(5000,5000);
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (b == null)
+                {
+                    b = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+                }
+            using (var g = Graphics.FromImage(b))
+            {
+                g.FillRectangle(Brushes.White, 0, 0, ClientRectangle.Width, ClientRectangle.Height);
+
+                g.FillEllipse(Brushes.Green, 100, 100, 100, 100);
+                g.FillRectangle(Brushes.Yellow, 250, 110, 50, 50);
+                g.FillEllipse(Brushes.Violet, 300, 300, 300, 420);
+                g.FillRectangle(Brushes.Pink, 250, 200, 150, 50);
+                //g.DrawEllipse(SystemPens.Control, old_x, old_y, 50, 50);
+                g.FillEllipse(Brushes.Blue, x, y, 50, 50);
+                //old_x = x;
+                //old_y = y;
+                
+                x+=krokx;
+                if ((x <= 0) || (( x ) >= (this.ClientRectangle.Width - 50)) )
+                {
+                    krokx = -krokx;  
+                }
+
+                y += kroky;
+                if ((y <= 0) || (( y ) >= (this.ClientRectangle.Height - 50)))
+                {
+                    kroky = -kroky;
+                }
+
+               
+
+            }//
+            
+            
+
+            using (Graphics gg = CreateGraphics())
+            {
+                gg.DrawImageUnscaled(b, 0, 0);
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void button2_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+         if (e.KeyCode == Keys.Up)
+            {
+                krokx += Math.Sign(krokx) * 1;
+                
+            }
+         if (e.KeyCode == Keys.Down)
+            {
+                krokx -= Math.Sign(krokx) * 1;
+            }
+         if (e.KeyCode == Keys.Left)
+            {
+                kroky -= Math.Sign(kroky) * 1;
+            }
+         if (e.KeyCode == Keys.Right)
+            {
+                
+                kroky += Math.Sign(kroky) * 1;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
